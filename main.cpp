9a5148934aee08e62e6568a83b5c25edbeb7b7d8
Wleaf666 <1617844769@qq.com>
2026-02-23 08:52:31 +0800

    feast:渲染了一副新图片

--- main.cpp
+++ main.cpp
@@ -22,6 +22,57 @@
 //     else
 //         return (-b - sqrt(discriminant)) / (2.0 * a);
 // }
+hittable_list random_scene()
+{
+    using namespace std;
+    hittable_list world;
+
+    world.add(make_shared<sphere>(
+        vec3(0, -1000, 0), 1000, make_shared<lambertian>(vec3(0.5, 0.5, 0.5))));
+
+    int i = 1;
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            auto choose_mat = random_double();
+            vec3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9)
+            {
+                if (choose_mat < 0.8)
+                {
+                    // diffuse
+                    auto albedo = vec3::random() * vec3::random();
+                    world.add(
+                        make_shared<sphere>(center, 0.2, make_shared<lambertian>(albedo)));
+                }
+                else if (choose_mat < 0.95)
+                {
+                    // metal
+                    auto albedo = vec3::random(.5, 1);
+                    auto fuzz = random_double(0, .5);
+                    world.add(
+                        make_shared<sphere>(center, 0.2, make_shared<metal>(albedo, fuzz)));
+                }
+                else
+                {
+                    // glass
+                    world.add(make_shared<sphere>(center, 0.2, make_shared<dielectric>(1.5)));
+                }
+            }
+        }
+    }
+
+    world.add(make_shared<sphere>(vec3(0, 1, 0), 1.0, make_shared<dielectric>(1.5)));
+
+    world.add(
+        make_shared<sphere>(vec3(-4, 1, 0), 1.0, make_shared<lambertian>(vec3(0.4, 0.2, 0.1))));
+
+    world.add(
+        make_shared<sphere>(vec3(4, 1, 0), 1.0, make_shared<metal>(vec3(0.7, 0.6, 0.5), 0.0)));
+
+    return world;
+}
 
 vec3 ray_color(const ray &r, const hittable &world,int depth)
 {
@@ -51,30 +102,31 @@ vec3 ray_color(const ray &r, const hittable &world,int depth)
 int main()
 {
     freopen("image.ppm", "w", stdout);
-    const int image_width = 600;
-    const int image_height = 300;
-    const int samples_per_pixel = 100;
+    const int image_width = 1000;
+    const int image_height =500;
+    const int samples_per_pixel = 80;
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
     vec3 horizontal(4.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
     int max_depth = 50;
     double aspect_ratio = double(image_width) / image_height;
-    vec3 lookfrom(2, 2, 1);
-    vec3 lookat(0, 0, -1);
+    vec3 lookfrom(13, 2, 3);
+    vec3 lookat(0, 0, 0);
     vec3 vup(0, 1, 0);
-    auto dist_to_focus = (lookfrom - lookat).length();
-    camera cam(60, aspect_ratio, vec3(2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0), 0.2, (lookfrom - lookat).length());
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+    camera cam(20, aspect_ratio,lookfrom, lookat, vup, aperture, 10.0);
 
-    hittable_list world;
-    world.add(std::make_shared<sphere>(vec3(0, 0, -1), 0.5, std::make_shared<lambertian>(vec3(0.7, 0.3, 0.3))));
-    world.add(std::make_shared<sphere>(vec3(0, -100.5, -1), 100,std::make_shared<lambertian>(vec3(0.5,0.5,0.5))));
+    hittable_list world=random_scene();
+    // world.add(std::make_shared<sphere>(vec3(0, 0, -1), 0.5, std::make_shared<lambertian>(vec3(0.7, 0.3, 0.3))));
+    // world.add(std::make_shared<sphere>(vec3(0, -100.5, -1), 100,std::make_shared<lambertian>(vec3(0.5,0.5,0.5))));
 
-    world.add(std::make_shared<sphere>(vec3(1, 0, -1), 0.5, std::make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.3)));
+    // world.add(std::make_shared<sphere>(vec3(1, 0, -1), 0.5, std::make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.3)));
+    // // world.add(std::make_shared<sphere>(vec3(-1, 0, -1), 0.5, std::make_shared<dielectric>(1.5)));
+
     // world.add(std::make_shared<sphere>(vec3(-1, 0, -1), 0.5, std::make_shared<dielectric>(1.5)));
-
-    world.add(std::make_shared<sphere>(vec3(-1, 0, -1), 0.5, std::make_shared<dielectric>(1.5)));
-    world.add(std::make_shared<sphere>(vec3(-1, 0, -1), -0.45, std::make_shared<dielectric>(1.5)));
+    // world.add(std::make_shared<sphere>(vec3(-1, 0, -1), -0.45, std::make_shared<dielectric>(1.5)));
     std::cout << "P3\n"
               << image_width << ' ' << image_height << "\n255\n";
     for (int j = image_height - 1; j >= 0; j--)
