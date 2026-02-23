3ec2a196b42b497e5843c03ad7c608e93b2829a1
Wleaf666 <1617844769@qq.com>
2026-02-23 12:10:04 +0800

    fix:修复了漫反射极端情况产生黑点

--- material.h
+++ material.h
@@ -24,6 +24,10 @@ class lambertian : public material
         virtual bool scatter(const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered)const
         {
             vec3 scatter_direction = rec.normal + random_unit_vector();
+            if (scatter_direction.near_zero())
+            {
+                scatter_direction = rec.normal;
+            }
             scattered = ray(rec.p, scatter_direction);
             attenuation = albedo;
             return true;
