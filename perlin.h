acd7f7b8c4f3220490ed5f62198ed609277b98de
Wleaf666 <1617844769@qq.com>
2026-02-27 11:26:35 +0800

    feast:使用了线性插值的柏林噪声

--- perlin.h
+++ perlin.h
@@ -36,6 +36,22 @@ class perlin
             }
         }
 
+        static double trilinear_interp(double c[2][2][2],double u,double v,double w)
+        {
+            double accum = 0.0;
+            for (int i = 0; i < 2;i++)
+            {
+                for (int j = 0; j < 2;j++)
+                {
+                    for (int k = 0; k < 2;k++)
+                    {
+                        accum += (i * u + (1 - i) * (1 - u)) * (j * v + (1 - j) * (1 - v)) * (k * w + (1 - k) * (1 - w))*c[i][j][k] ;
+                    }
+                }
+            }
+            return accum;
+        }
+
         public:
             perlin()
             {
@@ -60,10 +76,30 @@ class perlin
 
             double noise(const vec3& p)const
             {
-                int i = static_cast<int>(4 * p.x()) & 255;
-                int j = static_cast<int>(4 * p.y()) & 255;
-                int k = static_cast<int>(4 * p.z()) & 255;
-                return ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+                auto u = p.x() - floor(p.x());
+                auto v = p.y()-floor(p.y());
+                auto w = p.z() - floor(p.z());
+
+                u = u * u * (3 - 2 * u);
+                v = v * v * (3 - 2 * v);
+                w = w * w * (3 - 2 * w);
+
+                int i = static_cast<int>(floor(p.x()));
+                int j = static_cast<int>(floor(p.y()));
+                int k = static_cast<int>(floor(p.z()));
+                double c[2][2][2];
+
+                for (int di = 0; di < 2; di++)
+                {
+                    for (int dj = 0; dj < 2;dj++)
+                    {
+                        for (int dk = 0; dk < 2;dk++)
+                        {
+                            c[di][dj][dk] = ranfloat[perm_x[(i + di) & 255] ^ perm_y[(j + dj) & 255] ^ perm_z[(k + dk) & 255]];
+                        }
+                    }
+                }
+                return trilinear_interp(c, u, v, w);
             }
 
 
